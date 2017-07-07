339df57bc853e567bc481ba45da68da540be9253
keerthanaduraisamy <keerthana09297@gmail.com>
2017-07-07 15:28:29 +0530

    Create multi.java

---
+++ multi.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+public class Multi{
+  public static void main(String args[])
+  {
+    Scanner sc=new Scanner(System.in);
+    int n,i,x;
+    System.out.println("enter the number ");
+    n=sc.nextInt();
+    for(i=1;i<=10;i++)
+    {
+       x=i*n;
+      System.out.println(i+" X " +n+ " = "+x);
+      }
+      }
+      } 
